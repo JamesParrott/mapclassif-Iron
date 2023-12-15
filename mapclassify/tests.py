4c0e14b1d99b42baf1c2f72a09f7ea3f6bd8082f
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2023-12-15 11:43:00 +0000

    Update tests.py

--- mapclassify/tests.py
+++ mapclassify/tests.py
@@ -1,2 +1,15 @@
 import mapclassify
 
+class TestFisherJenks:
+    def setup_method(self):
+        self.V = load_example()
+
+    def test_FisherJenks(self):
+        fj = FisherJenks(self.V)
+        assert fj.adcm == 799.24000000000001
+        numpy.testing.assert_array_almost_equal(
+            fj.bins, numpy.array([75.29, 192.05, 370.5, 722.85, 4111.45])
+        )
+        numpy.testing.assert_array_almost_equal(
+            fj.counts, numpy.array([49, 3, 4, 1, 1])
+        )
